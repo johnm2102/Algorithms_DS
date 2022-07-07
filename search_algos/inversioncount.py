360d4cdef2cd1eeb879b9f5423fd50300d5310c1
johnm2102 <tln507@alumni.ku.dk>
2022-07-07 14:51:40 +0530

    added some stuff

--- search_algos/inversioncount.py
+++ search_algos/inversioncount.py
@@ -1,19 +1,20 @@
 '''
 Inversion Counter: 
     Counts the number of inversions in an array
+    Uses mergesort 
 '''
 
-def inversioncount(array, x): 
+def inversioncount(ar, x): 
     counter = 0
-    for i in range(x):
+    for i in range(x): 
         for j in range(i + 1, x):
-            if (array[i] > array[j]):
-                counter += 1
-    return counter 
+            if (ar[i] > ar[j]): 
+                counter  += 1 
+    return counter
         
 
 
-array_1 = [6,2,1,6,4,8,10]
-x = len(array_1)
+ar = [6,2,1,6,4,8,10]
+x = len(ar)
 
-print(inversioncount(array_1,x))
+print(inversioncount(ar,x))
